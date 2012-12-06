1b66d2156acbaf784ec4eb02708668b85dfe795b
Zach <Zjones44@yahoo.com>
2012-12-05 19:22:36 -0800

    media player should work

--- EPdemicsApp/gen/com/example/epdemicsapp/R.java
+++ EPdemicsApp/gen/com/example/epdemicsapp/R.java
@@ -14,27 +14,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070003;
-        public static final int button3=0x7f070002;
-        public static final int button4=0x7f070000;
-        public static final int menu_settings=0x7f070004;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f080000;
+        public static final int menu_settings=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int ep_player=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int test_cbr=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int band_info=0x7f040004;
-        public static final int ep_player=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int profile=0x7f040006;
-        public static final int stations_genres=0x7f040005;
+        public static final int app_name=0x7f050000;
+        public static final int band_info=0x7f050004;
+        public static final int ep_player=0x7f050003;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int profile=0x7f050006;
+        public static final int stations_genres=0x7f050005;
     }
     public static final class style {
         /** 
@@ -58,10 +61,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- EPdemicsApp/src/com/example/epdemicsapp/epActivity.java
+++ EPdemicsApp/src/com/example/epdemicsapp/epActivity.java
@@ -1,6 +1,7 @@
 package com.example.epdemicsapp;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class epActivity extends Activity 
@@ -10,5 +11,9 @@ public class epActivity extends Activity
 	{
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.ep_player);
+	
+	
+	MediaPlayer mp = MediaPlayer.create(this, R.raw.test_cbr);
+	mp.start();
 	}
 }
