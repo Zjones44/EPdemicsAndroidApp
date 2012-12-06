af269d78ae69632bdc39770e43bfe8dadf8450e0
Zach <Zjones44@yahoo.com>
2012-12-05 18:00:11 -0800

    hope this tests well

--- EPdemicsApp/src/com/example/epdemicsapp/MainActivity.java
+++ EPdemicsApp/src/com/example/epdemicsapp/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.epdemicsapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btnSimple = (Button) findViewById(R.id.button1);
+        btnSimple.setOnClickListener(new OnClickListener()
+        {
+        	public void onClick(View v){
+        		
+        		Intent intent = new Intent (v.getContext(), epActivity.class);
+        		startActivityForResult(intent, 0);
+        	}
+
+			
+        	
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+
 }
---
+++ EPdemicsApp/src/com/example/epdemicsapp/epActivity.java
@@ -0,0 +1,14 @@
+package com.example.epdemicsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class epActivity extends Activity 
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.ep_player);
+	}
+}
